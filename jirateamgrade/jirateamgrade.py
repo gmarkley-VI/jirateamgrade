69fde1c0025bd9521eaedae02d3fe6976046972f
gmarkley-VI <gmarkley@redhat.com>
2020-05-04 16:40:31 -0400

    Inital Commit

---
+++ jirateamgrade/jirateamgrade.py
@@ -0,0 +1,10 @@
+def grade(my_name):
+    """
+    Print a line about converting a notebook.
+    Args:
+        my_name (str): person's name
+    Returns:
+        None
+    """
+
+    print("I'll grade your project you some day, {my_name}.")
