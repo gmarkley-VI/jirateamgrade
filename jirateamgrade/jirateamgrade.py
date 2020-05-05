f7b466c1e8c357b2fc6dd9ad1584a822e6ccb4dc
gmarkley-VI <gmarkley@redhat.com>
2020-05-05 11:27:05 -0400

    Implemented Score the same as jirraCommentScore GOlang.

--- jirateamgrade/jirateamgrade.py
+++ jirateamgrade/jirateamgrade.py
@@ -1,5 +1,7 @@
 from jira.client import JIRA
 from pprint import pprint as print
+from collections import defaultdict, Counter
+import nltk
 import logging
 import os
 
@@ -18,6 +20,37 @@ def connect_jira(log, jira_server, jira_user, jira_password):
         log.error("Failed to connect to JIRA: %s" % e)
         return None
 
+# Defines a function for scoring comments in Jira
+def scoreComment(text):
+
+    # Count all sentences from all documents
+    sentences = nltk.sent_tokenize(text)
+
+    # tag speech
+    tokens = nltk.word_tokenize(text)
+    tagged_words = nltk.pos_tag(tokens)
+
+    # Count nouns
+    freqs = defaultdict(int)
+
+    for type in tagged_words:
+        if 'NN' in type[1]:
+            freqs['noun'] += 1
+        if 'VB' in type[1]:
+            freqs['verb'] += 1
+
+# Count Entities
+    entities = nltk.chunk.ne_chunk(tagged_words, binary=True)
+    named_entities = []
+
+    for t in entities.subtrees():
+        if t.label() == 'NE':
+            named_entities.append(t)
+
+    score = len(named_entities)*10 + len(sentences)*2.5 + freqs.get('noun') + freqs.get('verb')
+
+    return score
+
 def main():
     # create logger
     log = logging.getLogger(__name__)
@@ -28,8 +61,10 @@ def main():
     # create a connection object, jc
     jc = connect_jira(log, 'https://issues.redhat.com', username, passwd)
 
-    issue = jc.issue('SPLAT-8')
-    print(issue.fields.description)
+    issue = jc.issue('SPLAT-8').fields.description
+
+    print(issue)
+    print(scoreComment(issue))
 
 if __name__ == "__main__":
     main()
